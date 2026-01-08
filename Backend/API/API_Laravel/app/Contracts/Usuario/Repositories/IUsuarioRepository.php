bd1bb8bc2fbb56ba5f33677487fa006e3e595044
Br4h7an005 <126094502+Br4h7an005@users.noreply.github.com>
2026-01-08 13:02:19 -0500

    Delete Backend/API directory

--- Backend/API/API_Laravel/app/Contracts/Usuario/Repositories/IUsuarioRepository.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Contracts\Usuario\Repositories;
-
-interface IUsuarioRepository
-{
-    public function findById(int $id);
-    public function update(int $id,array $data);
-}
