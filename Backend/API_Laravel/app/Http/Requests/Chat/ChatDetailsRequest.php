3d983a202d3d8ce19f9348d67b0d60b31cb2f4d7
Brahian Cortes Ceballos <brahiancortes1200@gmail.com>
2026-01-24 16:03:29 -0500

    feat: Request para detalles de un chat

--- Backend/API_Laravel/app/Http/Requests/Chat/ChatDetailsRequest.php
+++ Backend/API_Laravel/app/Http/Requests/Chat/ChatDetailsRequest.php
@@ -7,9 +7,6 @@ use Illuminate\Support\Facades\Auth;
 
 class ChatDetailsRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
         return Auth::check();
