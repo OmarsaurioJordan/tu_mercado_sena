9977e5ee4b633555c6eddf19d7f4e8ee0a92ca66
Brahian Cortes Ceballos <brahiancortes1200@gmail.com>
2026-02-23 21:18:53 -0500

    feat: Inclusión de la columna tipo en la migración de motivos

--- Backend/API_Laravel/database/migrations/2026_01_27_010550_create_motivos_table.php
+++ Backend/API_Laravel/database/migrations/2026_01_27_010550_create_motivos_table.php
@@ -15,108 +15,159 @@ return new class extends Migration
         Schema::create('motivos', function (Blueprint $table) {
             $table->id();
             $table->string('nombre', 32);
+            $table->string('tipo', 32);
             $table->string('descripcion', 128);
         });
 
         DB::table('motivos')->insert([
             [
-                'nombre' => 'pqrs_pregunta',
+                // id: 1
+                'nombre' => 'pregunta',
+                'tipo' => 'pqrs',
                 'descripcion' => 'mensaje de pregunta'
             ],
             [
-                'nombre' => 'pqrs_queja',
+                // id: 2
+                'nombre' => 'queja',
+                'tipo' => 'pqrs',
                 'descripcion' => 'mensaje de queja'
             ],
             [
-                'nombre' => 'pqrs_reclamo',
+                // id: 3
+                'nombre' => 'reclamo',
+                'tipo' => 'pqrs',
                 'descripcion' => 'mensaje de reclamo'
             ],
             [
-                'nombre' => 'pqrs_sugerencia',
+                //id: 4
+                'nombre' => 'sugerencia',
+                'tipo' => 'pqrs',
                 'descripcion' => 'mensaje de sugerencia'
             ],
             [
-                'nombre' => 'pqrs_agradecimiento',
+                // id: 5
+                'nombre' => 'agradecimiento',
+                'tipo' => 'pqrs',
                 'descripcion' => 'mensaje de agradecimiento'
             ],
             [
+                // id: 6
                 'nombre' => 'notifica_denuncia',
+                'tipo' => 'notificacion',
                 'descripcion' => 'se ha respondido algo ante una denuncia'
             ],
             [
-                'nombre' => 'notifica_pqrs',
+                // id: 7
+                'nombre' => 'pqrs',
+                'tipo' => 'notificacion',
                 'descripcion' => 'se ha respondido algo a una PQRS'
             ],
             [
-                'nombre' => 'notifica_comprador',
+                // id: 8
+                'nombre' => 'comprador',
+                'tipo' => 'notificacion',
                 'descripcion' => 'un comprador potencial se ha puesto en contacto'
             ],
             [
+                // id: 9
                 'nombre' => 'notifica_comunidad',
+                'tipo' => 'notificacion',
                 'descripcion' => 'ha llegado un mensaje enviado a todos los usuarios'
             ],
             [
-                'nombre' => 'notifica_administrativa',
+                // id: 10
+                'nombre' => 'administrativa',
+                'tipo' => 'notificacion',
                 'descripcion' => 'un mensaje interno de la administración, por ejemplo, puedes haber sido baneado o eliminado'
             ],
             [
-                'nombre' => 'notifica_bienvenida',
+                // id: 11
+                'nombre' => 'bienvenida',
+                'tipo' => 'notificacion',
                 'descripcion' => 'mensaje de bienvenida al sistema'
             ],
             [
-                'nombre' => 'notifica_oferta',
+                // id: 12
+                'nombre' => 'oferta',
+                'tipo' => 'notificacion',
                 'descripcion' => 'un favorito ha publicado un nuevo producto'
             ],
             [
-                'nombre' => 'notifica_venta',
+                // id: 13
+                'nombre' => 'venta',
+                'tipo' => 'notificacion',
                 'descripcion' => 'un vendedor ha enviado una solicitud de consolidar venta'
             ],
             [
-                'nombre' => 'notifica_devolver',
+                // id: 14
+                'nombre' => 'devolver',
+                'tipo' => 'notificacion',
                 'descripcion' => 'un comprador ha enviado una solicitud de cancelar una transacción'
             ],
             [
-                'nombre' => 'notifica_exito',
+                // id: 15
+                'nombre' => 'exito',
+                'tipo' => 'notificacion',
                 'descripcion' => 'se ha llevado a cabo una compraventa exitosa'
             ],
             [
-                'nombre' => 'notifica_cancela',
+                // id: 16
+                'nombre' => 'cancela',
+                'tipo' => 'notificacion',
                 'descripcion' => 'se ha llevado a cabo una devolución exitosa, se cancelará la compraventa del historial'
             ],
             [
+                // id: 17
                 'nombre' => 'notifica_califica',
+                'tipo' => 'notificacion',
                 'descripcion' => 'un comprador ha calificado o escrito un comentario, o lo ha modificado'
             ],
             [
-                'nombre' => 'denuncia_acoso',
+                // id: 18
+                'nombre' => 'acoso',
+                'tipo' => 'denuncia',
                 'descripcion' => 'comportamiento de acoso sexual en un chat o imágenes o descripciónes'
             ],
             [
-                'nombre' => 'denuncia_bulling',
+                // id: 19
+                'nombre' => 'bulling',
+                'tipo' => 'denuncia',
                 'descripcion' => 'comportamiento de burlas o insultos en un chat o imágenes o descripciónes'
             ],
             [
-                'nombre' => 'denuncia_violencia',
+                // id: 20
+                'nombre' => 'violencia',
+                'tipo' => 'denuncia',
                 'descripcion' => 'comportamiento que incita al odio o amenzada directamente'
             ],
             [
-                'nombre' => 'denuncia_ilegal',
+                // id: 21
+                'nombre' => 'ilegal',
+                'tipo' => 'denuncia',
                 'descripcion' => 'comportamiento asociado a drogas, armas, prostitución y demás'
             ],
             [
-                'nombre' => 'denuncia_troll',
+                // id: 22
+                'nombre' => 'troll',
+                'tipo' => 'denuncia',
                 'descripcion' => 'comportamiento enfocado en molestar y hacer perder el tiempo, por ejemplo, con negociaciónes por mamar gallo'
             ],
             [
-                'nombre' => 'denuncia_fraude',
+                // id: 23
+                'nombre' => 'fraude',
+                'tipo' => 'denuncia',
                 'descripcion' => 'se trata de vender algo malo o mediante trampas, tratan de tumbar al otro con fraudes'
             ],
             [
-                'nombre' => 'denuncia_fake',
+                // id: 24
+                'nombre' => 'fake',
+                'tipo' => 'denuncia',
                 'descripcion' => 'un producto o perfil es meme o chisto o simplemente hace perder el tiempo al no ser una propuesta real'
             ],
             [
-                'nombre' => 'denuncia_spam',
+                // id: 25
+                'nombre' => 'spam',
+                'tipo' => 'denuncia',
                 'descripcion' => 'un producto o perfil aparece muchas veces como si lo pusieran en demasia para llamar la atención'
             ],
         ]);
